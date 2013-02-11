6c935b785a08ce633771e5dd05035656f2d4d080
John Shutt <john.d.shutt@gmail.com>
2013-02-10 19:25:14 -0800

    describe bot in comments

---
+++ nondescriptbot.py
@@ -0,0 +1,17 @@
+# Search Open Library for books that don't have descriptions yet.
+
+
+
+# Search Wikipedia to find out which books in the list have entries.
+
+
+
+# For the books that have Wikipedia entries, take the first paragraph of 
+# the entry and make that their Open Library description.
+
+
+
+# Save all of the new book descriptions.
+
+
+
